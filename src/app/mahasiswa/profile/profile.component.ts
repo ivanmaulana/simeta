28d6cdccf55f01106c2c8a5b5b3bfa3db96ad23a
ivanmaulana <ivanmaulanaputra@gmail.com>
2017-01-10 22:03:34 +0700

    add profile

--- src/app/mahasiswa/profile/profile.component.ts
+++ src/app/mahasiswa/profile/profile.component.ts
@@ -1,14 +1,97 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ViewEncapsulation, NgZone} from '@angular/core';
+import {AuthHttp} from 'angular2-jwt';
+
+import { ToastrService } from 'toastr-ng2';
+import { DataService } from '../../data/data.service';
+
+import { NgUploaderOptions } from 'ngx-uploader';
 
 @Component({
   selector: 'profile',
   encapsulation: ViewEncapsulation.None,
+  providers: [DataService],
   styles: [require('./profile.scss')],
   template: require('./profile.html')
 })
 export class Profile {
+  // status
+  statusDaftar;
+  statusKolokium;
+  statusPraseminar;
+  statusProfile;
+  statusSeminar;
+  statusSidang;
+  statusSkl;
+  statusTa;
 
-  constructor() {
+  // cek koneksi
+  noConn;
+  status;
+
+  private nim;
+  private nama;
+
+  constructor(public authHttp: AuthHttp, public toastr: ToastrService, public data: DataService) {
+
+
   }
+
+  // -----------------------------
+  // TEMPLATE
+
+  // DASHBOARD SERVICE
+  getStatus() {
+    this.authHttp.get(this.data.urlStatus)
+      .map(res => res.json())
+      .subscribe( data => {
+        this.statusDaftar = data[0].statusDaftar;
+        this.statusKolokium = data[0].statusKolokium;
+        this.statusPraseminar = data[0].statusPrasminar;
+        this.statusTa = data[0].statusTa;
+        this.statusSeminar = data[0].statusSeminar;
+        this.statusSidang = data[0].statusSidang;
+        this.statusSkl = data[0].statusSkl;
+        this.statusProfile = data[0].statusProfile;
+
+        // console.log('status TA'+this.statusTa);
+
+        if(this.statusTa) {
+          // this.getDataMahasiswa();
+        }
+      })
+  }
+
+  ngOnInit() {
+    this.getStatus();
+    this.getConnection();
+  }
+
+  getConnection() {
+    this.noConn = 0;
+
+    this.authHttp.get(this.data.urlTest)
+      .map(res => res.json())
+      .subscribe(data => {
+        this.status = data['status'];
+      })
+
+    setTimeout(() => {
+      if (!this.status) {
+        this.status = 0;
+        this.noConn = 1;
+        this.showNoConn();
+      }
+    }, 5000)
+  }
+
+  refresh() {
+    this.getConnection();
+    this.getStatus();
+  }
+
+  showNoConn() {
+    this.toastr.warning("Error Connecting to Server", 'Error');
+  }
+
 
 }
