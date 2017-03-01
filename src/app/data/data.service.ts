5cbc37b8256d032ab1743e36542f1261548f3bea
ivanmaulana <ivanmaulanaputra@gmail.com>
2017-03-01 09:48:33 +0700

    pre release

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -39,87 +39,87 @@ export class DataService {
   private aaa = 'aaa';
 
   // URL
-  public urlTa = 'http://localhost:2016/ta/';
-  public urlDataPengajuan = 'http://localhost:2016/ta/daftar/';
-  public urlDosen = 'http://localhost:2016/dosen/';
-  public urlDaftarPengajuan = 'http://localhost:2016/ta/pengajuan/';
-  public urlProfile = 'http://localhost:2016/profile/';
-  public urlSidang = 'http://localhost:2016/sidang/';
-  public urlStatus = 'http://localhost:2016/status/';
-  public urlTest = 'http://localhost:2016/test/';
-  public urlLog = 'http://localhost:2016/log/';
-  public urlSKL = 'http://localhost:2016/skl/';
-  public urlKolokium = 'http://localhost:2016/jadwalKolokium';
-  public urlPraseminar = 'http://localhost:2016/jadwalPraseminar';
+  public urlTa = 'http://simeta-api.apps.cs.ipb.ac.id/ta/';
+  public urlDataPengajuan = 'http://simeta-api.apps.cs.ipb.ac.id/ta/daftar/';
+  public urlDosen = 'http://simeta-api.apps.cs.ipb.ac.id/dosen/';
+  public urlDaftarPengajuan = 'http://simeta-api.apps.cs.ipb.ac.id/ta/pengajuan/';
+  public urlProfile = 'http://simeta-api.apps.cs.ipb.ac.id/profile/';
+  public urlSidang = 'http://simeta-api.apps.cs.ipb.ac.id/sidang/';
+  public urlStatus = 'http://simeta-api.apps.cs.ipb.ac.id/status/';
+  public urlTest = 'http://simeta-api.apps.cs.ipb.ac.id/test/';
+  public urlLog = 'http://simeta-api.apps.cs.ipb.ac.id/log/';
+  public urlSKL = 'http://simeta-api.apps.cs.ipb.ac.id/skl/';
+  public urlKolokium = 'http://simeta-api.apps.cs.ipb.ac.id/jadwalKolokium';
+  public urlPraseminar = 'http://simeta-api.apps.cs.ipb.ac.id/jadwalPraseminar';
 
-  public urlJadwalMicon = 'http://localhost:2016/jadwalMicon';
+  public urlJadwalMicon = 'http://simeta-api.apps.cs.ipb.ac.id/jadwalMicon';
 
-  public urlUpdateTa = 'http://localhost:2016/ta/update/';
-  public urlTaPengajuan = 'http://localhost:2016/ta/daftar/detail/';
+  public urlUpdateTa = 'http://simeta-api.apps.cs.ipb.ac.id/ta/update/';
+  public urlTaPengajuan = 'http://simeta-api.apps.cs.ipb.ac.id/ta/daftar/detail/';
 
-  public urlTaPenentuan = 'http://localhost:2016/ta/penentuan';
-  public urlTaEdit = 'http://localhost:2016/ta/edit/';
+  public urlTaPenentuan = 'http://simeta-api.apps.cs.ipb.ac.id/ta/penentuan';
+  public urlTaEdit = 'http://simeta-api.apps.cs.ipb.ac.id/ta/edit/';
 
-  public urlJadwalKolokium = 'http://localhost:2016/jadwalKolokium';
+  public urlJadwalKolokium = 'http://simeta-api.apps.cs.ipb.ac.id/jadwalKolokium';
 
-  public urlFileKolokium = 'http://localhost:2016/kolokium';
-  public urlFilePraseminar = 'http://localhost:2016/praseminar';
+  public urlFileKolokium = 'http://simeta-api.apps.cs.ipb.ac.id/kolokium';
+  public urlFilePraseminar = 'http://simeta-api.apps.cs.ipb.ac.id/praseminar';
 
 
-  public urlUploadKolokium = 'http://localhost:2016/upload/kolokium';
-  public urlUploadPraseminar = 'http://localhost:2016/upload/praseminar';
-  public urlUploadSKL = 'http://localhost:2016/upload/skl';
-  public urlUploadTAFinal = 'http://localhost:2016/upload/final';
-  public urlUploadPhoto = 'http://localhost:2016/upload/photo';
-  public urlUploadJadwalKolokium = 'http://localhost:2016/upload/jadwal/kolokium';
-  public urlUploadJadwalPraseminar = 'http://localhost:2016/upload/jadwal/praseminar';
-  public urlUploadJadwalMicon = 'http://localhost:2016/upload/jadwal/micon';
+  public urlUploadKolokium = 'http://simeta-api.apps.cs.ipb.ac.id/upload/kolokium';
+  public urlUploadPraseminar = 'http://simeta-api.apps.cs.ipb.ac.id/upload/praseminar';
+  public urlUploadSKL = 'http://simeta-api.apps.cs.ipb.ac.id/upload/skl';
+  public urlUploadTAFinal = 'http://simeta-api.apps.cs.ipb.ac.id/upload/final';
+  public urlUploadPhoto = 'http://simeta-api.apps.cs.ipb.ac.id/upload/photo';
+  public urlUploadJadwalKolokium = 'http://simeta-api.apps.cs.ipb.ac.id/upload/jadwal/kolokium';
+  public urlUploadJadwalPraseminar = 'http://simeta-api.apps.cs.ipb.ac.id/upload/jadwal/praseminar';
+  public urlUploadJadwalMicon = 'http://simeta-api.apps.cs.ipb.ac.id/upload/jadwal/micon';
 
-  public urlUploadMandiri = 'http://localhost:2016/upload/seminar/mandiri';
-  public urlUploadKonferensi = 'http://localhost:2016/upload/seminar/konferensi';
-  public urlUploadMicon = 'http://localhost:2016/upload/seminar/micon';
-  public urlUploadSidang = 'http://localhost:2016/upload/sidang';
+  public urlUploadMandiri = 'http://simeta-api.apps.cs.ipb.ac.id/upload/seminar/mandiri';
+  public urlUploadKonferensi = 'http://simeta-api.apps.cs.ipb.ac.id/upload/seminar/konferensi';
+  public urlUploadMicon = 'http://simeta-api.apps.cs.ipb.ac.id/upload/seminar/micon';
+  public urlUploadSidang = 'http://simeta-api.apps.cs.ipb.ac.id/upload/sidang';
 
-  public urlLogApproval = 'http://localhost:2016/log/approval';
-  public urlApproveLog = 'http://localhost:2016/log/approve';
-  public urlDeleteLog = 'http://localhost:2016/log/delete';
+  public urlLogApproval = 'http://simeta-api.apps.cs.ipb.ac.id/log/approval';
+  public urlApproveLog = 'http://simeta-api.apps.cs.ipb.ac.id/log/approve';
+  public urlDeleteLog = 'http://simeta-api.apps.cs.ipb.ac.id/log/delete';
 
-  public urlAllMakalahKolokium = 'http://localhost:2016/mahasiswa/kolokium';
-  public urlAllMakalahKolokiumDosen = 'http://localhost:2016/mahasiswa/kolokium/dosen';
-  public urlAllMakalahPraseminar = 'http://localhost:2016/mahasiswa/praseminar';
-  public urlAllMakalahPraseminarDosen = 'http://localhost:2016/mahasiswa/praseminar/dosen';
-  public urlAllMakalahSidang = 'http://localhost:2016/mahasiswa/sidang';
-  public urlAllMakalahSidangDosen = 'http://localhost:2016/mahasiswa/sidang/dosen';
-  public urlAllMakalahSKL = 'http://localhost:2016/mahasiswa/skl';
-  public urlAllMakalahSKLDosen = 'http://localhost:2016/mahasiswa/skl/dosen';
-  public urlAllMakalahBimbingan = 'http://localhost:2016/mahasiswa/bimbingan';
+  public urlAllMakalahKolokium = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/kolokium';
+  public urlAllMakalahKolokiumDosen = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/kolokium/dosen';
+  public urlAllMakalahPraseminar = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/praseminar';
+  public urlAllMakalahPraseminarDosen = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/praseminar/dosen';
+  public urlAllMakalahSidang = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/sidang';
+  public urlAllMakalahSidangDosen = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/sidang/dosen';
+  public urlAllMakalahSKL = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/skl';
+  public urlAllMakalahSKLDosen = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/skl/dosen';
+  public urlAllMakalahBimbingan = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/bimbingan';
 
-  public urlAdminKolokium = 'http://localhost:2016/admin/kolokium';
-  public urlAdminPraseminar = 'http://localhost:2016/admin/praseminar';
-  public urlAdminSeminar = 'http://localhost:2016/admin/seminar';
-  public urlAdminSidang = 'http://localhost:2016/admin/sidang';
-  public urlAdminSKL = 'http://localhost:2016/admin/skl';
-  public urlAdminSummary = 'http://localhost:2016/admin/summary';
-  public urlDosenSummary = 'http://localhost:2016/dosen/summary';
+  public urlAdminKolokium = 'http://simeta-api.apps.cs.ipb.ac.id/admin/kolokium';
+  public urlAdminPraseminar = 'http://simeta-api.apps.cs.ipb.ac.id/admin/praseminar';
+  public urlAdminSeminar = 'http://simeta-api.apps.cs.ipb.ac.id/admin/seminar';
+  public urlAdminSidang = 'http://simeta-api.apps.cs.ipb.ac.id/admin/sidang';
+  public urlAdminSKL = 'http://simeta-api.apps.cs.ipb.ac.id/admin/skl';
+  public urlAdminSummary = 'http://simeta-api.apps.cs.ipb.ac.id/admin/summary';
+  public urlDosenSummary = 'http://simeta-api.apps.cs.ipb.ac.id/dosen/summary';
 
-  public urlDosenKolokium = 'http://localhost:2016/dosen/kolokium';
-  public urlDosenPraseminar = 'http://localhost:2016/dosen/praseminar';
-  public urlDosenSeminar = 'http://localhost:2016/dosen/seminar';
-  public urlDosenSidang = 'http://localhost:2016/dosen/sidang';
-  public urlDosenSKL = 'http://localhost:2016/dosen/skl';
+  public urlDosenKolokium = 'http://simeta-api.apps.cs.ipb.ac.id/dosen/kolokium';
+  public urlDosenPraseminar = 'http://simeta-api.apps.cs.ipb.ac.id/dosen/praseminar';
+  public urlDosenSeminar = 'http://simeta-api.apps.cs.ipb.ac.id/dosen/seminar';
+  public urlDosenSidang = 'http://simeta-api.apps.cs.ipb.ac.id/dosen/sidang';
+  public urlDosenSKL = 'http://simeta-api.apps.cs.ipb.ac.id/dosen/skl';
 
-  public urlSeminarData = 'http://localhost:2016/seminar';
-  public urlSeminarMandiri = 'http://localhost:2016/seminar/mandiri';
-  public urlSeminarKonferensi = 'http://localhost:2016/seminar/konferensi';
+  public urlSeminarData = 'http://simeta-api.apps.cs.ipb.ac.id/seminar';
+  public urlSeminarMandiri = 'http://simeta-api.apps.cs.ipb.ac.id/seminar/mandiri';
+  public urlSeminarKonferensi = 'http://simeta-api.apps.cs.ipb.ac.id/seminar/konferensi';
 
-  public urlSeminarAll = 'http://localhost:2016/mahasiswa/seminar/all';
-  public urlSeminarDosen = 'http://localhost:2016/mahasiswa/seminar/dosen';
+  public urlSeminarAll = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/seminar/all';
+  public urlSeminarDosen = 'http://simeta-api.apps.cs.ipb.ac.id/mahasiswa/seminar/dosen';
 
-  public urlDeleteSeminar = 'http://localhost:2016/seminar/delete';
+  public urlDeleteSeminar = 'http://simeta-api.apps.cs.ipb.ac.id/seminar/delete';
 
 
   // ADMIN
-  public urlListPengajuan = 'http://localhost:2016/ta/daftar/list';
+  public urlListPengajuan = 'http://simeta-api.apps.cs.ipb.ac.id/ta/daftar/list';
 
   public send = 1;
   jwtHelper: JwtHelper = new JwtHelper();
